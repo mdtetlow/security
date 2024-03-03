0fde09cf54764234d0315694d378217a703bb1b8
Mark tetlow <mdtetlow@gmail.com>
2024-03-03 16:41:58 +0000

    Add HTTP lab 1

--- labs/other/http/http_01.py
+++ labs/other/http/http_01.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python3
-from http_client import message
+from http import message
 import argparse
 import socket
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--host', help='Hostname excluding protocal', required=True)
+parser.add_argument('--host', required=True)
 parser.add_argument('--port', type=int, default=80, required=False)
 
 args = parser.parse_args()
 
 message = message.create_http_message('GET',
-                                           f"http://{args.host}/pentesterlab",
-                                           {'Host': args.host})
+                                      f"http://{args.host}/pentesterlab")
+
+print(message)
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((args.host, args.port))
