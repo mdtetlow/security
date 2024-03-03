84c8123e49726e3653e6879aea79c52760149562
Mark tetlow <mdtetlow@gmail.com>
2024-03-03 16:36:32 +0000

    Add HTTP Python Package

---
+++ libs/python/packages/http/message.py
@@ -0,0 +1,96 @@
+"""HTML Message creation helper
+
+Supports creating of RFC-9110 compliant HTTP Messages
+
+RFC-9110: https://www.rfc-editor.org/rfc/rfc9110.html
+Mozilla HTTP Messages: https://developer.mozilla.org/en-US/docs/Web/HTTP/Messages
+"""
+
+from http.url import HTTPUrl
+
+def http_request_line(method: str,
+                      path: str,
+                      version: str):
+    """Create HTTP Request Line format string
+
+    Parameters
+    ----------
+    method : str
+        HTTP method according to RFC-9110
+        GET, PUT, HEAD, POST, DELETE, CONNECT, OPTIONS
+    path : str
+        HTTP resource path
+    version : str
+        HTTP version (1.1, 2.0, 3.0)
+    """
+
+    return f"{method} {path} HTTP/{version}\r\n"
+
+def http_header_section(headers: dict):
+    """Creates HTTP Headers section (including Cookies) according to RFC-9110
+
+    Parameters
+    ----------
+    headers : dict
+        A dictionary object containing the Headers and Cookies as key,value pairs
+    """
+
+    return "\r\n".join(f'{k}: {v}' for k,v in headers.items()) + "\r\n"
+
+def http_body_section(body: list):
+    """Creates HTTP Body according to RFC-9110
+
+    Parameters
+    ----------
+    body : list
+        List of string lines making up the HTTP Body according to RFC-9110
+    """
+
+    return f"{body}"
+
+def required_headers(version: str,
+                     host: str,
+                     body: str = None):
+    headers: dict = {'Accept': '*/*'}
+
+    if version == '1.1':
+        headers.update({'Host': host})
+    
+    if body:
+        headers.update(
+            {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Content-Length': len(body.encode('utf-8'))
+            })
+
+    return headers
+
+def create_http_message(method: str,
+                        url: str,
+                        headers: dict = {},
+                        body: str = None,
+                        version: str = "1.1"):
+    """Create HTTP Message according to RFC-9110
+
+    Parameters
+    ----------
+    method : str
+        HTTP method according to RFC-9110
+        GET, PUT, HEAD, POST, DELETE, CONNECT, OPTIONS
+    path : str
+        HTTP resource path
+    headers : dict
+        A dictionary object containing the Headers and Cookies as key,value pairs
+    body : list
+        List of string lines making up the HTTP Body according to RFC-9110
+    version : str
+        HTTP version (1.1, 2.0, 3.0)
+    """
+    url_obj = HTTPUrl(url)
+    request_line = http_request_line(method, url_obj.path(), version)
+    headers_section = {**required_headers(version, url_obj.host(), body), **headers} # input argument headers overrides required headers values
+    http_message = f"{request_line}{http_header_section(headers_section)}\r\n"
+    if method == 'POST' or body:
+        http_message = f"{http_message}{http_body_section(body)}"
+    
+    return http_message
---
+++ libs/python/packages/http/url.py
@@ -0,0 +1,46 @@
+"""HTTP URL helper
+"""
+
+from urllib.parse import urlparse
+
+class HTTPUrl:
+
+    def __init__(self, url):
+        self.url = urlparse(url)
+
+    def __str__(self) -> str:
+        return f""
+    
+    def inet_address(self):
+        return self.__inet_address_from_url()
+    
+    def port(self):
+        # get inet_addr tuple (host,port)
+        addr = self.__inet_address_from_url()
+        return addr[1]
+    
+    def host(self):
+        return self.url.netloc.split(':')[0] # extract host from [host:port]
+    
+    def path(self):
+        path = self.url.path + f"?{self.url.query}" if self.url.query else self.url.path
+        return path
+    
+    def query(self):
+        return self.url.query
+    
+    # private methods
+    def __inet_address_from_url(self):
+        """Socket AF_INET Address from HTTP URL
+
+        Parameters
+        ----------
+        url : str
+            HTTP format URL
+        """
+        port: int = self.url.port
+
+        if not port:
+            port = 80 if self.url.scheme == 'http' else 443
+        
+        return (self.host(), port)
